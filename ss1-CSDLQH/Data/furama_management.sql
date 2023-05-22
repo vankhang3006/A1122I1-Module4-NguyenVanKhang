627c99adf533e8375b1aed02776c905f9436ea6e
vankhang3006 <119319324+vankhang3006@users.noreply.github.com>
2023-05-22 19:10:05 +0700

    sda

---
+++ ss1-CSDLQH/Data/furama_management.sql
@@ -0,0 +1,289 @@
+create database furama_management;
+use furama_management;
+
+create table vi_tri(
+	ma_vi_tri int auto_increment primary key,
+    ten_vi_tri nvarchar(45) not null
+);
+
+create table trinh_do(
+	ma_trinh_do int auto_increment primary key,
+    ten_trinh_do nvarchar(45) not null
+);
+
+create table bo_phan(
+	ma_bo_phan int auto_increment primary key,
+    ten_bo_phan nvarchar(45)
+);
+
+create table nhan_vien(
+	ma_nhan_vien int auto_increment primary key,
+    ho_ten nvarchar(255) not null,
+    ngay_sinh date not null,
+    so_cmnd double not null unique,
+    luong double not null,
+    so_dien_thoai varchar(10) not null unique,
+    email varchar(255) not null unique,
+    dia_chi nvarchar(255) not null,
+    ma_vi_tri int not null,
+    ma_trinh_do int not null,
+    ma_bo_phan int not null,
+    foreign key(ma_vi_tri) references vi_tri(ma_vi_tri),
+    foreign key(ma_trinh_do) references trinh_do(ma_trinh_do),
+    foreign key(ma_bo_phan) references bo_phan(ma_bo_phan)
+);
+create table loai_khach(
+	ma_loai_khach int auto_increment primary key,
+    ten_loai_khach varchar(45) not null
+);   
+
+create table khach_hang(
+	ma_khach_hang int auto_increment primary key,
+    ho_ten nvarchar(255) not null,
+    ngay_sinh date not null,
+    gioi_tinh bit default 1,
+    so_cmnd double not null unique,
+    so_dien_thoai varchar(10) not null unique,
+    email varchar(255) not null unique,
+    dia_chi nvarchar(255) not null,
+    ma_loai_khach int not null,
+    foreign key(ma_loai_khach) references loai_khach(ma_loai_khach)
+);
+
+create table kieu_thue(
+	ma_kieu_thue int auto_increment primary key,
+    ten_kieu_thue varchar(10) not null
+);
+
+create table loai_dich_vu(
+	ma_loai_dich_vu int auto_increment primary key,
+    ten_loai_dich_vu varchar(10) not null
+);
+
+create table dich_vu(
+	ma_dich_vu int auto_increment primary key,
+    ten_dich_vu varchar(255) not null,
+    dien_tich double not null,
+    chi_phi_thue double not null,
+    so_nguoi_toi_da int not null,
+    tieu_chuan_phong varchar(10) not null,
+    mo_ta_tien_nghi_khac nvarchar(255) not null,
+    dien_tich_ho_boi int null,
+    so_tang int null,
+    ma_kieu_thue int not null,
+    ma_loai_dich_vu int not null,
+    foreign key(ma_kieu_thue) references kieu_thue(ma_kieu_thue),
+    foreign key(ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu)
+);
+
+create table dich_vu_di_kem(
+	ma_dich_vu_di_kem int auto_increment primary key,
+    ten_dich_vu_di_kem nvarchar(255) not null,
+    gia double not null,
+    don_vi nvarchar(10) not null,
+    trang_thai nvarchar(255) not null
+);
+
+create table hop_dong(
+	ma_hop_dong int auto_increment primary key,
+    ngay_lam_hop_dong date not null,
+    ngay_ket_thuc date not null,
+    tien_dat_coc double default 0,
+    ma_nhan_vien int not null,
+    ma_khach_hang int not null,
+    ma_dich_vu int not null,
+    foreign key(ma_nhan_vien) references nhan_vien(ma_nhan_vien),
+    foreign key(ma_khach_hang) references khach_hang(ma_khach_hang),
+    foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
+);
+
+create table hop_dong_chi_tiet(
+	ma_hop_dong_chi_tiet int auto_increment primary key,
+    so_luong int not null,
+    ma_hop_dong int not null,
+    ma_dich_vu_di_kem int not null,
+    foreign key(ma_hop_dong) references hop_dong(ma_hop_dong),
+    foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem)
+);
+insert into vi_tri(ten_vi_tri) values	('Quản Lý'),
+										('Nhân Viên');
+                                        
+insert into trinh_do(ten_trinh_do) values	('Trung Cấp'),
+											('Cao Đẳng'),
+                                            ('Đại Học'),
+                                            ('Sau Đại Học');
+                                            
+insert into bo_phan(ten_bo_phan) values	('Sale-Marketing'),
+										('Hành chính'),
+                                        ('Phục vụ'),
+                                        ('Quản lý');
+                                        
+insert into nhan_vien(ho_ten, ngay_sinh, so_cmnd, luong, so_dien_thoai, email, dia_chi, ma_vi_tri, ma_trinh_do, ma_bo_phan) values	('Nguyễn Văn An', '1970-11-07', 456231786, 10000000, '0901234121', 'annguyen@gmail.com', '295 Nguyễn Tất Thành, Đà Nẵng',1,3,1),
+																																	('Lê Văn Bình', '1997-04-09', 654231234, 7000000, '0934212314', 'binhlv@gmail.com', '22 Yên Bái, Đà Nẵng', 1, 2, 2),
+																																	('Hồ Thị Yến', '1995-12-12', 999231723, 14000000, '0412352315', 'thiyen@gmail.com', 'K234/11 Điện Biên Phủ, Gia Lai', 1, 3, 2),
+																																	('Võ Công Toản', '1980-04-04', 123231365, 17000000, '0374443232', 'toan0404@gmail.com', '77 Hoàng Diệu, Quảng Trị', 1, 4, 4),
+																																	('Nguyễn Bỉnh Phát', '1999-12-09', 454363232, 6000000, '0902341231', 'phatphat@gmail.com', '43 Yên Bái, Đà Nẵng', 2, 1, 1),
+																																	('Khúc Nguyễn An Nghi', '2000-11-08', 964542311, 7000000, '0978653213', 'annghi20@gmail.com', '294 Nguyễn Tất Thành, Đà Nẵng', 2, 2, 3),
+																																	('Nguyễn Hữu Hà', '1993-01-01', 534323231, 8000000, '0941234553', 'nhh0101@gmail.com', '4 Nguyễn Chí Thanh, Huế', 2, 3, 2),
+																																	('Nguyễn Hà Đông', '1989-09-03', 234414123, 9000000, '0642123111', 'donghanguyen@gmail.com', '111 Hùng Vương, Hà Nội', 2, 4, 4),
+																																	('Tòng Hoang', '1982-09-03', 256781231, 6000000, '0245144444', 'hoangtong@gmail.com', '213 Hàm Nghi, Đà Nẵng', 2, 4, 4),
+																																	('Nguyễn Công Đạo', '1994-01-08', 755434343, 8000000, '0988767111', 'nguyencongdao12@gmail.com', '6 Hoà Khánh, Đồng Nai', 2, 3, 2);
+                                                                                                                                    
+insert into loai_khach(ten_loai_khach) values	('Diamond'),
+												('Platinum'),
+                                                ('Gold'),
+                                                ('Silver'),
+                                                ('Member');
+											
+insert into khach_hang(ho_ten, ngay_sinh, gioi_tinh, so_cmnd, so_dien_thoai, email, dia_chi, ma_loai_khach) values	('Nguyễn Thị Hào', '1970-11-07', 0, 643431213, '0945423362', 'thihao07@gmail.com', '23 Nguyễn Hoàng, Đà Nẵng', 5),
+																													('Phạm Xuân Diệu', '1992-08-08', 1, 865342123, '0954333333', 'xuandieu92@gmail.com', 'K77/22 Thái Phiên, Quảng Trị', 3),
+                                                                                                                    ('Trương Đình Nghệ', '1990-02-27', 1, 488645199, '0373213122', 'nghenhan2702@gmail.com', 'K323/12 Ông Ích Khiêm, Vinh', 1),
+                                                                                                                    ('Dương Văn Quan', '1981-07-08', 1, 543432111, '0490039241', 'duongquan@gmail.com', 'K453/12 Lê Lợi, Đà Nẵng', 1),
+                                                                                                                    ('Hoàng Trần Nhi Nhi', '1995-12-09', 0, 795453345, '0312345678', 'nhinhi123@gmail.com', '224 Lý Thái Tổ, Gia Lai', 4),
+                                                                                                                    ('Tôn Nữ Mộc Châu', '2005-12-06', 0, 732434215, '0988888844', 'tonnuchau@gmail.com', '37 Yên Thế, Đà Nẵng', 4),
+                                                                                                                    ('Nguyễn Mỹ Kim', '1984-04-08', 0, 856453123, '0912345698', 'kimcuong84@gmail.com', 'K123/45 Lê Lợi, Hồ Chí Minh', 1),
+                                                                                                                    ('Nguyễn Thị Hào', '1999-04-08', 0, 965656433, '0763212345', 'haohao99@gmail.com', '55 Nguyễn Văn Linh, Kon Tum', 3),
+                                                                                                                    ('Trần Đại Danh', '1994-07-01', 1, 432341235, '0643343433', 'danhhai99@gmail.com', '24 Lý Thường Kiệt, Quảng Ngãi', 1),
+                                                                                                                    ('Nguyễn Tâm Đắc', '1989-07-01', 1, 344343432, '0987654321', 'dactam@gmail.com', '22 Ngô Quyền, Đà Nẵng', 2);
+																															
+insert into kieu_thue(ten_kieu_thue) values	('year'),
+											('month'),
+                                            ('day'),
+                                            ('hour');
+                                            
+insert into loai_dich_vu(ten_loai_dich_vu) values	('Villa'),
+													('House'),
+                                                    ('Room');
+												
+insert into dich_vu(ten_dich_vu, dien_tich, chi_phi_thue, so_nguoi_toi_da, tieu_chuan_phong, mo_ta_tien_nghi_khac, dien_tich_ho_boi, so_tang, ma_kieu_thue, ma_loai_dich_vu) 
+values	('Villa Beach Front', 25000, 10000000, 10, 'vip', 'Có hồ bơi', 500, 4, 3, 1),
+		('House Princess 01', 14000, 5000000, 7, 'vip', 'Có thêm bếp nướng', NULL, 3, 2, 2),
+        ('Room Twin 01', 5000, 1000000, 2, 'normal', 'Có tivi', NULL, NULL , 4, 3),
+        ('Villa No Beach Front', 22000, 9000000, 8, 'normal', 'Có hồ bơi', 300, 3, 3, 1),
+        ('House Princess 02', 10000, 4000000, 5, 'normal', 'Có thêm bếp nướng', NULL, 2, 3, 2),
+        ('Room Twin 02', 3000, 900000, 2, 'normal', 'Có tivi', NULL, NULL, 4, 3);
+        
+insert into dich_vu_di_kem(ten_dich_vu_di_kem, gia, don_vi, trang_thai)
+values	('Karaoke', 10000, 'giờ', 'tiện nghi, hiện tại'),
+		('Thuê xe máy', 10000, 'chiếc', 'hỏng 1 xe'),
+        ('Thuê xe đạp', 20000, 'chiếc', 'tốt'),
+        ('Buffet buổi sáng', 15000, 'suất', 'đầy đủ đồ ăn, tráng miệng'),
+        ('Buffet buổi trưa', 90000, 'suất', 'đầy đủ đồ ăn, tráng miệng'),
+        ('Buffet buổi tối', 16000, 'suất', 'đầy đủ đồ ăn, tráng miệng');
+        
+insert into hop_dong(ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, ma_nhan_vien, ma_khach_hang, ma_dich_vu)
+values	('2020-12-08', '2020-12-08', 0, 3, 1, 3),
+		('2020-07-14', '2020-07-21', 200000, 7, 3, 1),
+        ('2021-03-15', '2021-03-17', 50000,	3, 4, 2),
+        ('2021-01-14',	'2021-01-18',	100000,	7,	5,	5),
+		('2021-07-14',	'2021-07-15',	0,	7,	2,	6),
+		('2021-06-01',	'2021-06-03',	0,	7,	7,	6),
+		('2021-09-02',	'2021-09-05',	100000,	7,	4,	4),
+		('2021-06-17',	'2021-06-18',	150000,	3,	4,	1),
+		('2020-11-19',	'2020-11-19',	0,	3,	4,	3),
+		('2021-04-12',	'2021-04-14',	0,	10,	3,	5),
+		('2021-04-25',	'2021-04-25',	0,	2,	2,	1),
+		('2021-05-25',	'2021-05-27',	0,	7,	10,	1);
+        
+insert into hop_dong_chi_tiet(so_luong, ma_hop_dong, ma_dich_vu_di_kem)
+values	(5,	2, 4),
+		(8,	2, 5),
+		(15, 2,	6),
+		(1,	3, 1),
+		(11, 3,	2),
+		(1,	1, 3),
+		(2,	1, 2),
+		(2,	12,	2);
+-- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+select * from nhan_vien where ho_ten like 'h%' or ho_ten like 't%' or ho_ten like 'k%' and char_length(ho_ten) <=15;
+-- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+select * from khach_hang where (timestampdiff(year, ngay_sinh, curdate()) between 18 and 50) and (dia_chi like '%Đà Nẵng' or dia_chi like '%Quảng Trị');
+-- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+select kh.ma_khach_hang, kh.ho_ten, count(hd.ma_khach_hang) as so_lan_dat_phong from khach_hang kh, hop_dong hd, loai_khach lk 
+where kh.ma_khach_hang = hd.ma_khach_hang and kh.ma_loai_khach = lk.ma_loai_khach and lk.ten_loai_khach = 'Diamond'
+group by kh.ma_khach_hang order by count(hd.ma_khach_hang) asc;
+-- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+SELECT kh.ma_khach_hang, kh.ho_ten, kh.ho_ten, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, IFNULL(t.totalCPT + IFNULL(SUM(hdct.so_luong * dvdk.gia), 0),0) total FROM khach_hang kh
+LEFT JOIN hop_dong hd USING(ma_khach_hang)
+LEFT JOIN hop_dong_chi_tiet hdct USING(ma_hop_dong)
+LEFT JOIN dich_vu_di_kem dvdk USING(ma_dich_vu_di_kem)
+LEFT JOIN dich_vu dv USING(ma_dich_vu)
+LEFT JOIN
+(
+SELECT hd.ma_hop_dong ,IFNULL(SUM(dv.chi_phi_thue), 0)  totalCPT FROM khach_hang kh
+LEFT JOIN hop_dong hd USING(ma_khach_hang)
+LEFT JOIN dich_vu dv USING(ma_dich_vu)
+GROUP BY kh.ma_khach_hang
+) t USING(ma_hop_dong)
+GROUP BY kh.ma_khach_hang ;
+-- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+FROM dich_vu dv
+LEFT JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+WHERE dv.ma_dich_vu NOT IN (
+    SELECT hd.ma_dich_vu
+    FROM hop_dong hd
+    WHERE hd.ngay_lam_hop_dong >= '2021-01-01' AND hd.ngay_lam_hop_dong <= '2021-03-31'
+)
+ORDER BY dv.ma_dich_vu ASC;
+-- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.so_nguoi_toi_da, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+FROM dich_vu dv
+LEFT JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+WHERE dv.ma_dich_vu IN (
+    SELECT hd.ma_dich_vu FROM hop_dong hd
+    WHERE hd.ngay_lam_hop_dong >= '2020-01-01' AND hd.ngay_lam_hop_dong <= '2020-12-31'
+)
+AND dv.ma_dich_vu NOT IN (
+    SELECT hd.ma_dich_vu FROM hop_dong hd
+    WHERE hd.ngay_lam_hop_dong >= '2021-01-01' AND hd.ngay_lam_hop_dong <= '2021-12-31'
+)
+ORDER BY dv.ma_dich_vu ASC;
+-- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+select ho_ten from khach_hang group by ho_ten;
+-- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+SELECT MONTH(ngay_lam_hop_dong) AS thang, COUNT(DISTINCT ma_khach_hang) as so_luong_khach_hang
+FROM hop_dong
+WHERE YEAR(ngay_lam_hop_dong) = 2021
+GROUP BY MONTH(ngay_lam_hop_dong)
+ORDER BY thang ASC;
+-- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+SELECT hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc, SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem
+FROM hop_dong hd
+JOIN hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+GROUP BY hdct.ma_hop_dong
+ORDER BY hd.ma_hop_dong asc;
+-- 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+SELECT DISTINCT dich_vu_di_kem.ma_dich_vu_di_kem, dich_vu_di_kem.ten_dich_vu_di_kem
+FROM dich_vu_di_kem
+INNER JOIN hop_dong_chi_tiet ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+INNER JOIN hop_dong ON hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+INNER JOIN khach_hang ON hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+INNER JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+WHERE loai_khach.ten_loai_khach = 'Diamond' 
+    AND (khach_hang.dia_chi LIKE '%Vinh%' OR khach_hang.dia_chi LIKE '%Quảng Ngãi%');
+-- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+SELECT 
+    hd.ma_hop_dong, 
+    nv.ho_ten AS ho_ten_nhan_vien, 
+    kh.ho_ten AS ho_ten_khach_hang, 
+    kh.so_dien_thoai AS SDT_khach_hang,
+    dv.ma_dich_vu,
+    dv.ten_dich_vu,
+    SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem,
+    hd.tien_dat_coc
+FROM hop_dong hd
+INNER JOIN nhan_vien nv ON hd.ma_nhan_vien = nv.ma_nhan_vien
+INNER JOIN khach_hang kh ON hd.ma_khach_hang = kh.ma_khach_hang
+INNER JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+INNER JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+INNER JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+WHERE 
+    MONTH(hd.ngay_ket_thuc) BETWEEN 10 AND 12 AND YEAR(hd.ngay_ket_thuc) = 2020 AND
+    hd.ma_hop_dong NOT IN (
+        SELECT hd.ma_hop_dong 
+        FROM hop_dong hd
+        WHERE MONTH(hd.ngay_ket_thuc) < 6 AND YEAR(hd.ngay_ket_thuc) = 2021
+    )
+GROUP BY hd.ma_hop_dong;
