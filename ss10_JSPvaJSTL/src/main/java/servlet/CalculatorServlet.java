d179214f66118dc3f0a2408202cd46e080514d0e
vankhang3006 <119319324+vankhang3006@users.noreply.github.com>
2023-06-03 00:01:24 +0700

    ss10_JSPvaJSTL

--- ss10_JSPvaJSTL/src/main/java/servlet/CalculatorServlet.java
+++ ss10_JSPvaJSTL/src/main/java/servlet/CalculatorServlet.java
@@ -1,3 +1,7 @@
+package servlet;
+
+import model.Calculator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -6,21 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "DiscountServlet", urlPatterns = "/calculate")
+@WebServlet(name = "CalculatorServlet", urlPatterns = "/calculate")
 public class CalculatorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String price = request.getParameter("price");
-        String discount = request.getParameter("discount");
-
-        double discountAmount = Double.parseDouble(price) * Double.parseDouble(discount) * 0.01;
-        double discountPrice = Double.parseDouble(price) - discountAmount;
-
+        float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
+        float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
+        char operator = request.getParameter("operator").charAt(0);
         PrintWriter writer = response.getWriter();
         writer.println("<html>");
-
-        writer.println("<h2>Discount Amount: " + Double.toString(discountAmount) + "</h2>");
-        writer.println("<h2>Discount Price: " + Double.toString(discountPrice) + "</h2>");
-
+        writer.println("<h1>Result:</h1>");
+        try{
+            float result = Calculator.calculate(firstOperand, secondOperand, operator);
+            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+        }catch (Exception ex){
+            writer.println("Error: " + ex.getMessage());
+        }
         writer.println("</html>");
     }
 
